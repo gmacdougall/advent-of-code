78637bacef9cd2151f6e1914b23e2917b13ae36b
Gregor MacDougall <gmacdougall0+github@gmail.com>
2018-12-05 08:30:31 -0500

    December 4 solutions

---
+++ 2018/04/part2.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+def minute(line)
+  line.match(/\d{4}-\d{2}-\d{2} \d{2}:(\d{2})/)[1].to_i
+end
+
+guard_schedule = Hash.new { |h, k| h[k] = Array.new(60, 0) }
+
+guard, sleep_start = nil
+
+ARGF.each do |line|
+  if line.include?('begins shift')
+    guard = line.match(/Guard #(\d+) begins shift/)[1].to_i
+  elsif line.include?('falls asleep')
+    sleep_start = minute(line)
+  elsif line.include?('wakes up')
+    sleep_end = minute(line)
+    (sleep_start...sleep_end).each do |minute|
+      guard_schedule[guard][minute] += 1
+    end
+  end
+end
+
+sleepiest_guard = guard_schedule.max_by { |k,v| v.max }.first
+sleepiest_minute = guard_schedule[sleepiest_guard].each_with_index.max[1]
+
+puts "#{sleepiest_guard} * #{sleepiest_minute} = #{sleepiest_guard * sleepiest_minute}"
