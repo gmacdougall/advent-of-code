48c21f6258f52e33c5f5d54260740dd443e8630c
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-12-04 22:53:44 -0500

    Minor refactoring

--- 2021/04/part2.rb
+++ 2021/04/part2.rb
@@ -2,19 +2,19 @@
 # frozen_string_literal: true
 
 input = ARGF.read.split("\n").map(&:strip).reject(&:empty?)
-cards = input[1..].map { |a| a.split(/\s+/).map(&:to_i) }.each_slice(5).to_a
-numbers = input.first.split(',').map(&:to_i)
+numbers = input.shift.split(',').map(&:to_i)
+cards = input.map { |a| a.split(/\s+/).map(&:to_i) }.each_slice(5).to_a
 loser = nil
 
 numbers.each_with_index do |n, idx|
   selected = numbers[0..idx]
   cards.reject! do |card|
     [card, card.transpose].any? do |c|
-      c.any? { |row| (row & selected).length == 5 }
+      c.any? { |row| (row & selected) == row }
     end
   end
 
-  loser = cards.first if !loser && cards.length == 1
+  loser = cards.first if !loser && cards.one?
   next if cards.any?
 
   puts n * (loser.flatten - selected).sum
