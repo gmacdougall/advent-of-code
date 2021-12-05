8fc633195dc185196b0e3113bf1373a22cda4d08
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-12-05 11:41:31 -0500

    Minor refactoring

--- 2021/05/part2.rb
+++ 2021/05/part2.rb
@@ -4,27 +4,25 @@
 input = ARGF.each_line.map { |line| line.match(/(\d+),(\d+) -> (\d+),(\d+)/).to_a.last(4).map(&:to_i) }
 map = Hash.new(0)
 
+def enumerate(val1, val2)
+  val1.public_send(val1 < val2 ? :upto : :downto, val2).to_a
+end
+
 orthoganal_lines = input.select { |l| l[0] == l[2] || l[1] == l[3] }
 orthoganal_lines.each do |x1, y1, x2, y2|
-  Range.new(*[x1, x2].sort).each do |x|
-    Range.new(*[y1, y2].sort).each do |y|
+  enumerate(x1, x2).each do |x|
+    enumerate(y1, y2).each do |y|
       map[[x, y]] += 1
     end
   end
 end
 
-diagonal_lines = input.select { |l| [l[1] - l[3], l[3] - l[1]].include?(l[0] - l[2]) }
+diagonal_lines = input.select { |l| (l[1] - l[3]).abs == (l[0] - l[2]).abs }
 diagonal_lines.each do |x1, y1, x2, y2|
-  x = x1
-  y = y1
-  map[[x, y]] += 1
-
-  while x != x2
-    x += x2 > x ? 1 : -1
-    y += y2 > y ? 1 : -1
-
-    map[[x, y]] += 1
+  y_values = enumerate(y1, y2)
+  enumerate(x1, x2).each_with_index do |x, idx|
+    map[[x, y_values[idx]]] += 1
   end
 end
 
-puts(map.values.count { |n| (n || 0) > 1 })
+puts(map.values.count { |n| n && n > 1 })
