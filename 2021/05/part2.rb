816cad51c6098d44c070fb4c78767f980282a600
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-12-05 11:45:48 -0500

    More minor refactoring

--- 2021/05/part2.rb
+++ 2021/05/part2.rb
@@ -8,20 +8,18 @@ def enumerate(val1, val2)
   val1.public_send(val1 < val2 ? :upto : :downto, val2).to_a
 end
 
-orthoganal_lines = input.select { |l| l[0] == l[2] || l[1] == l[3] }
-orthoganal_lines.each do |x1, y1, x2, y2|
-  enumerate(x1, x2).each do |x|
-    enumerate(y1, y2).each do |y|
-      map[[x, y]] += 1
+input.each do |x1, y1, x2, y2|
+  if x1 == x2 || y1 == y2
+    enumerate(x1, x2).each do |x|
+      enumerate(y1, y2).each do |y|
+        map[[x, y]] += 1
+      end
     end
-  end
-end
-
-diagonal_lines = input.select { |l| (l[1] - l[3]).abs == (l[0] - l[2]).abs }
-diagonal_lines.each do |x1, y1, x2, y2|
-  y_values = enumerate(y1, y2)
-  enumerate(x1, x2).each_with_index do |x, idx|
-    map[[x, y_values[idx]]] += 1
+  elsif (x1 - x2).abs == (y1 - y2).abs
+    y_values = enumerate(y1, y2)
+    enumerate(x1, x2).each_with_index do |x, idx|
+      map[[x, y_values[idx]]] += 1
+    end
   end
 end
 
