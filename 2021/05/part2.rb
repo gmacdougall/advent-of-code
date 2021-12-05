6b8eb806f88d59fd42788e5efc86d88c2daf7c1e
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-12-05 14:36:48 -0500

    Even more refactoring

--- 2021/05/part2.rb
+++ 2021/05/part2.rb
@@ -9,18 +9,15 @@ def enumerate(start, finish)
 end
 
 input.each do |x1, y1, x2, y2|
-  if x1 == x2 || y1 == y2
-    enumerate(x1, x2).each do |x|
-      enumerate(y1, y2).each do |y|
-        map[[x, y]] += 1
-      end
-    end
+  if x1 == x2
+    enumerate(y1, y2).each { |y| map[[x1, y]] += 1 }
+  elsif y1 == y2
+    enumerate(x1, x2).each { |x| map[[x, y1]] += 1 }
   elsif (x1 - x2).abs == (y1 - y2).abs
-    y_values = enumerate(y1, y2)
-    enumerate(x1, x2).each_with_index do |x, idx|
-      map[[x, y_values[idx]]] += 1
+    enumerate(x1, x2).zip(enumerate(y1, y2)).each do |x, y|
+      map[[x, y]] += 1
     end
   end
 end
 
-puts(map.values.count { |n| n && n > 1 })
+puts(map.values.count { |n| n > 1 })
