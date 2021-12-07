b127349253c99de9e5bcff4f587702317315305c
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-12-07 08:10:44 -0500

    A little smaller

--- 2021/06/part2.golf.rb
+++ 2021/06/part2.golf.rb
@@ -1,2 +1,2 @@
 # Run with ruby -F, part2.golf.rb input
-a=[0]*9;$<.read.split.map{a[_1.to_i]+=1};256.times{a.rotate!;a[6]+=a[8]};p a.sum
+a=[0]*9;gets.split.map{a[_1.hex]+=1};256.times{a.rotate!;a[6]+=a[8]};p a.sum
