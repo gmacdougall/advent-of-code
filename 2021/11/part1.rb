00943315e9664204c367703cc3a497d331140e3d
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-12-11 07:11:23 -0500

    December 11, 2021

---
+++ 2021/11/part1.rb
@@ -0,0 +1,25 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+@state = ARGF.read.gsub("\n", '').to_i.digits
+
+def increment(idx)
+  x = idx % 10
+  y = idx / 10
+  @state[idx] += 1
+  (-1..1).each do |dx|
+    (-1..1).each do |dy|
+      next unless (0..9).cover?(x + dx) && (0..9).cover?(y + dy)
+
+      increment(idx + (dy * 10) + dx)
+    end
+  end if @state[idx] == 10
+end
+
+puts(
+  100.times.sum do |n|
+    100.times { increment _1 }
+    @state.map! { |n| n > 9 ? 0 : n }
+    @state.count(&:zero?)
+  end
+)
