f26f1439ad06562a72cd5279c99ca1944455eb96
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-12-12 08:46:05 -0500

    Rubocop updates

--- 2021/02/part1.rb
+++ 2021/02/part1.rb
@@ -3,7 +3,7 @@
 
 x = y = 0
 ARGF.each_line do |line|
-  action, num = line.split(' ')
+  action, num = line.split
   num = num.to_i
 
   case action
--- 2021/11/part2.rb
+++ 2021/11/part2.rb
@@ -7,17 +7,19 @@ def increment(idx)
   x = idx % 10
   y = idx / 10
   @state[idx] += 1
+  return unless @state[idx] == 10
+
   (-1..1).each do |dx|
     (-1..1).each do |dy|
       next unless (0..9).cover?(x + dx) && (0..9).cover?(y + dy)
 
       increment(idx + (dy * 10) + dx)
     end
-  end if @state[idx] == 10
+  end
 end
 
 puts(
-  (1..).find do |n|
+  (1..).find do
     100.times { increment _1 }
     @state.map! { |n| n > 9 ? 0 : n }
     @state.all?(&:zero?)
