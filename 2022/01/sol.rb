f50e8b10e4cb537f352b5a2e8ba71d79fcbfcd12
Gregor MacDougall <gmacdougall0+github@gmail.com>
2022-12-01 00:32:48 -0500

    Minor improvements Day 1

--- 2022/01/sol.rb
+++ 2022/01/sol.rb
@@ -1,5 +1,5 @@
 #! /usr/bin/env ruby
 
-totals = ARGF.read.split("\n\n").map { _1.split("\n").map(&:to_i) }.map(&:sum).sort
-puts "Part 1: #{totals.last}"
-puts "Part 2: #{totals.last(3).sum}"
+totals = ARGF.read.split("\n\n").map { _1.split("\n").map(&:to_i).sum }
+puts "Part 1: #{totals.max}"
+puts "Part 2: #{totals.max(3).sum}"
