b88de7ca08d628bcc640a23b4b19de737e1faad8
Gregor MacDougall <gmacdougall0+github@gmail.com>
2022-12-06 23:50:49 -0500

    Refined day 5
    It's quite easy!

--- 2022/06/sol.rb
+++ 2022/06/sol.rb
@@ -1,5 +1,4 @@
 #! /usr/bin/env ruby
 
-orig = ARGF.read.chars
-puts orig.each_cons(4).each_with_index.find { |c,idx| c.uniq.length == 4 }.last + 4
-puts orig.each_cons(14).each_with_index.find { |c,idx| c.uniq.length == 14 }.last + 14
+o = gets.chars
+[4,14].map{ |n| p n + o.each_cons(n).find_index{ _1.uniq == _1 } }
