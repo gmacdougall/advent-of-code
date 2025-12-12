b0998b30fe842841e726250105303c85ca47a824
Gregor MacDougall <gmacdougall0+github@gmail.com>
2025-12-12 01:34:55 -0500

    Fix the math for input presents

--- 2025/12/sol.rb
+++ 2025/12/sol.rb
@@ -3,16 +3,18 @@
 
 def parse(fname)
   input = File.read(fname).split("\n\n")
-  input.last.lines(chomp: true).map do |line|
+  presents = input.shift(6).map { it.lines.last(3).map { |l| l.strip.gsub('#', '1').gsub('.', '0').chars.map(&:to_i) }}
+  areas = input.first.lines(chomp: true).map do |line|
     area, foo = line.split(': ')
     [area.split('x').map(&:to_i), foo.split(' ').map(&:to_i)]
   end
+  [presents, areas]
 end
 
-def part1(areas)
+def part1(presents, areas)
   areas.count do |(x, y), requirements|
-    requirements.sum * 7 < x * y
+    requirements.each_with_index.sum { |req, idx| req * presents[idx].flatten.count { it == 1 } } < x * y
   end
 end
 
-puts "Part 1: #{part1(parse('input'))}"
+puts "Part 1: #{part1(*parse('input'))}"
